26563dc240769d3e4b2a89fd7a7ba62a36211bc4
Igor <igorv386@gmail.com>
2021-07-08 16:32:41 -0300

    exercicio to do List

--- to_do_list_js/js/index.js
+++ to_do_list_js/js/index.js
@@ -2,15 +2,16 @@ const listContainer = document.querySelector('[data-lists]')
 const newListForm = document.querySelector('[data-new-list-form]')
 const newListInput = document.querySelector('[data-new-list-input]')
 
-let list = []
+let lists = []
 /**
  * Toda tag form tem um comportamento padrão no navegador,
  * toda vez que o navegador recebe um submit a pagina é atualizada. 
  * Então eu preciso prevenir esse comportamento padrão pra que a nossa
  * página não perca os dados ao ser atualizada.
  */
+
 newListForm.addEventListener('submit', function(e){
-  e.addEventListener()
+  e.preventDefault()
   const listName = newListInput.value
   if (listName === null || listName === '') return
     const list = createList(listName)
@@ -19,8 +20,24 @@ newListForm.addEventListener('submit', function(e){
     render()
 })
 
+function createList(name){
+  return {id: Date.now().toString(), name: name }
+}
+
 function render(){
+  clearElement(listContainer)
   lists.forEach(function(list){
     const item = document.createElement('li')
+    item.classList.add('item')
+    item.innerText = list.name
+    listContainer.appendChild(item)
   })
 }
+
+function clearElement(element){
+  while(element.firstChild){
+    element.removeChild(element.firstChild)
+  }
+}
+
+render()
