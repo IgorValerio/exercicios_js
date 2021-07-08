d464299ea46dd35a6eaecf933df63add230effe2
Igor <igorv386@gmail.com>
2021-07-08 16:02:46 -0300

    exercicio convertToFahrenheit

--- reverse_str/script.js
+++ reverse_str/script.js
@@ -1,4 +1,3 @@
-Reverse String
 
 let newStr = '';
 
--- to_do_list_js/js/index.js
+++ to_do_list_js/js/index.js
@@ -1,55 +1,26 @@
-// var nome = "João";
-// var idade = 20;
-// var aprovado = true;
-// var alunos = ["Joao", "Kelly", "César"];
+const listContainer = document.querySelector('[data-lists]')
+const newListForm = document.querySelector('[data-new-list-form]')
+const newListInput = document.querySelector('[data-new-list-input]')
 
-// alunos.push("du");
-// var ultimoAluno = alunos.pop();
+let list = []
+/**
+ * Toda tag form tem um comportamento padrão no navegador,
+ * toda vez que o navegador recebe um submit a pagina é atualizada. 
+ * Então eu preciso prevenir esse comportamento padrão pra que a nossa
+ * página não perca os dados ao ser atualizada.
+ */
+newListForm.addEventListener('submit', function(e){
+  e.addEventListener()
+  const listName = newListInput.value
+  if (listName === null || listName === '') return
+    const list = createList(listName)
+    newListInput.value = null
+    lists.push(list)
+    render()
+})
 
-// console.log(nome);
-// console.log(idade);
-// console.log(aprovado);
-// console.log(alunos);
-// console.log("Ultimo aluno: " + ultimoAluno);
-
-// var aluno = {
-//     "nome": "João",
-//     "idade": 20,
-//     "aprovado": true,
-// };
-
-// aluno.nome = "Marcio";
-// aluno.nota = 7;
-
-// console.log(aluno);
-
-
-// if (aluno.nota > 5) {
-//     console.log(aluno.nome + " está aprovado!");
-// } else {
-//     console.log(aluno.nome + " está reprovado!");
-// }
-
-// if (aluno.nome != "Maria") {
-//     console.log("Aluno favorito!")
-// } else {
-//     console.log("Não é aluno favorito!")
-// }
-
-// //condição ? true : false
-// console.log(aluno.idade >= 18 ? "adulto" : "criança");
-
-
-
-
-
-
-//Convert to Fahrenheit
-
-// function convertToFahrenheit(value){
-//   return value * 1.8 + 32
-// }
-
-// let result = convertToFahrenheit(1)
-// console.log(`O valor em Fahrenheit é ${result}`)
-
+function render(){
+  lists.forEach(function(list){
+    const item = document.createElement('li')
+  })
+}
