17666d5433bbc5230b6dba0a62b27a661ddf795e
Igor <igorv386@gmail.com>
2021-07-08 11:46:36 -0300

    Exercicios reverse_str e iniciando o to_do_list_js

---
+++ to_do_list_js/js/index.js
@@ -0,0 +1,55 @@
+// var nome = "João";
+// var idade = 20;
+// var aprovado = true;
+// var alunos = ["Joao", "Kelly", "César"];
+
+// alunos.push("du");
+// var ultimoAluno = alunos.pop();
+
+// console.log(nome);
+// console.log(idade);
+// console.log(aprovado);
+// console.log(alunos);
+// console.log("Ultimo aluno: " + ultimoAluno);
+
+// var aluno = {
+//     "nome": "João",
+//     "idade": 20,
+//     "aprovado": true,
+// };
+
+// aluno.nome = "Marcio";
+// aluno.nota = 7;
+
+// console.log(aluno);
+
+
+// if (aluno.nota > 5) {
+//     console.log(aluno.nome + " está aprovado!");
+// } else {
+//     console.log(aluno.nome + " está reprovado!");
+// }
+
+// if (aluno.nome != "Maria") {
+//     console.log("Aluno favorito!")
+// } else {
+//     console.log("Não é aluno favorito!")
+// }
+
+// //condição ? true : false
+// console.log(aluno.idade >= 18 ? "adulto" : "criança");
+
+
+
+
+
+
+//Convert to Fahrenheit
+
+// function convertToFahrenheit(value){
+//   return value * 1.8 + 32
+// }
+
+// let result = convertToFahrenheit(1)
+// console.log(`O valor em Fahrenheit é ${result}`)
+
