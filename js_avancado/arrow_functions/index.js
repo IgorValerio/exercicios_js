e5286441dd6bcda2670c886f79d360ce64786d93
Igor <igorv386@gmail.com>
2021-07-09 16:20:26 -0300

    arrow function

---
+++ js_avancado/arrow_functions/index.js
@@ -0,0 +1,9 @@
+let soma = (num1, num2) => num1 + num2 // Quando tem apenas uma linha não precisa colocar chaves
+
+let ola = (nome) => {
+  `Hello: ${nome}`
+}
+
+let sayHello = name => `Hello ${name}` // Quando tem apenas 1 parãmetro não precisa colocar parênteses
+
+console.log(sayHello('Igor'))
